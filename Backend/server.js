fce278e70a22a53304e14c960069132517d2eaa1
Komal Mathamsetti <komalmathamsetti@gmail.com>
2026-06-25 19:49:56 +0530

    Initial project setup

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+const db = require("../Backend/config/db");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.get("/",(req,res)=>{
+    res.json({success:true,message:"Freelance marketplace API is running"});
+});
+const PORT = process.env.PORT||5000;
+app.listen(PORT,()=>{
+    console.log(`Server is ruuning on port ${PORT}`);
+});
