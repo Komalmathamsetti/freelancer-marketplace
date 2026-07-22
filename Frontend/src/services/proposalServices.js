f0290c3e81554db4b53fb43daa0a9219848788c1
Komal Mathamsetti <komalmathamsetti@gmail.com>
2026-07-22 15:31:52 +0530

    Made changes in routes

--- Frontend/src/services/proposalServices.js
+++ Frontend/src/services/proposalServices.js
@@ -2,7 +2,7 @@ import API from "./api";
 
 // Apply for a Job
 export const applyJob = (jobId, data) => {
-  return API.post(`/api/proposals/apply/${jobId}`, data, {
+  return API.post(`/proposals/apply/${jobId}`, data, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
@@ -11,7 +11,7 @@ export const applyJob = (jobId, data) => {
 
 // My Applications
 export const getMyApplications = () => {
-  return API.get("/api/proposals/my-applications", {
+  return API.get("/proposals/my-applications", {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
@@ -20,7 +20,7 @@ export const getMyApplications = () => {
 
 // View Applicants (Client)
 export const getApplicants = (jobId) => {
-  return API.get(`/api/proposals/applicants/${jobId}`, {
+  return API.get(`/proposals/applicants/${jobId}`, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
@@ -29,7 +29,7 @@ export const getApplicants = (jobId) => {
 
 // Accept Proposal
 export const acceptProposal = (proposalId) => {
-  return API.put(`/api/proposals/accept/${proposalId}`, {}, {
+  return API.put(`/proposals/accept/${proposalId}`, {}, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
@@ -38,28 +38,28 @@ export const acceptProposal = (proposalId) => {
 
 // Reject Proposal
 export const rejectProposal = (proposalId) => {
-  return API.put(`/api/proposals/reject/${proposalId}`, {}, {
+  return API.put(`/proposals/reject/${proposalId}`, {}, {
     headers: {
       Authorization: `Bearer ${localStorage.getItem("token")}`,
     },
   });
 };
 export const withdrawApplication = (proposalId) => {
-    return API.delete(`api/proposals/${proposalId}`, {
+    return API.delete(`/proposals/${proposalId}`, {
         headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
     });
 };
 export const checkApplication = (jobId) => {
-    return API.get(`/api/proposals/check/${jobId}`, {
+    return API.get(`/proposals/check/${jobId}`, {
         headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
     });
 };
 export const getClientProposals = () => {
-    return API.get("/api/proposals/client", {
+    return API.get("/proposals/client", {
         headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
         },
